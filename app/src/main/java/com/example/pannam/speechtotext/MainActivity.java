8871e0ee94e36086755c494f2a8fff8cafc93e14
jesussmile <pullbackwild@gmail.com>
2016-08-16 08:13:19 +0545

    Initial commit

---
+++ app/src/main/java/com/example/pannam/speechtotext/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.pannam.speechtotext;
+
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+TextView resultText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+    public void onButtonClick(View v){
+        if(v.getId()==R.id.imageButton)
+        {
+             resultText = (TextView)findViewById(R.id.TVresult);
+            promptSpeechInput();
+
+        }
+
+    }
+
+    private void promptSpeechInput() {
+
+        Intent i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        i.putExtra(RecognizerIntent.EXTRA_PROMPT,"Say  something");
+
+        try {
+
+            startActivityForResult(i, 100);
+        }
+        catch(ActivityNotFoundException a)
+        {
+            Toast.makeText(this, "Sorry your device doesn't support speech language!",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    public void onActivityResult(int request_code, int result_code, Intent i){
+        super.onActivityResult(request_code,result_code,i);
+
+        switch (request_code)
+        {
+            case 100: if(result_code == RESULT_OK & i != null){
+                ArrayList<String> result = i.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                resultText.setText(result.get(0));
+            }
+                break;
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
